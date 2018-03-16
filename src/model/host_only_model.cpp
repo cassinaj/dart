7725367f218f548dd5566652aa15b8c4f2336389
Issac <jissac@nvidia.com>
2018-03-16 13:44:36 -0700

    Replacing matheval expressions with simple std::stof. Expressions are not used in model description. If XML macros and expression evaluation is needed, use xacro to generate the desired XML description.

--- src/model/host_only_model.cpp
+++ src/model/host_only_model.cpp
@@ -35,25 +35,26 @@ HostOnlyModel::HostOnlyModel() : Model(6)
 HostOnlyModel::~HostOnlyModel() {}
 float HostOnlyModel::evaluateExpression(const std::string &expression)
 {
-    char *cStr = new char[expression.size() + 1];
-    memcpy(cStr, expression.c_str(), expression.size());
-    cStr[expression.size()] = 0;
-    void *f = evaluator_create(cStr);
-    assert(f);
-    char **varNames;
-    int varCount;
-    evaluator_get_variables(f, &varNames, &varCount);
-    double varValues[varCount];
-    for (int i = 0; i < varCount; ++i)
-    {
-        varValues[i] = getSizeParam(std::string(varNames[i]));
-    }
-    float val = evaluator_evaluate(f, varCount, varNames, varValues);
+    // char *cStr = new char[expression.size() + 1];
+    // memcpy(cStr, expression.c_str(), expression.size());
+    // cStr[expression.size()] = 0;
+    // void *f = evaluator_create(cStr);
+    // assert(f);
+    // char **varNames;
+    // int varCount;
+    // evaluator_get_variables(f, &varNames, &varCount);
+    // double varValues[varCount];
+    // for (int i = 0; i < varCount; ++i)
+    // {
+    //     varValues[i] = getSizeParam(std::string(varNames[i]));
+    // }
+    // float val = evaluator_evaluate(f, varCount, varNames, varValues);
 
-    delete[] cStr;
-    evaluator_destroy(f);
+    // delete[] cStr;
+    // evaluator_destroy(f);
 
-    return val;
+    // return val;
+    return std::stof(expression);
 }
 
 int HostOnlyModel::addFrame(int parent,
